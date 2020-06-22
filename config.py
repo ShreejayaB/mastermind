acecf02fb16b6208c622f2debeb5dc602121eca0
Nithish Kumar <nithishreddy777@gmail.com>
2020-06-21 18:31:16 -0700

    working q-learning

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-lstm_hidden_size = 2500
-feedback_embedding_size = 250
-guess_embedding_size = lstm_hidden_size - feedback_embedding_size
+lstm_hidden_size = 256
+feedback_embedding_size = 8
+guess_embedding_size = lstm_hidden_size - 2*feedback_embedding_size
 max_guesses = 6**4
 max_feedback = 15
 reinforce_alpha = 0.001
